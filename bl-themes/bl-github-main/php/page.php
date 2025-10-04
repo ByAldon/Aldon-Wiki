657d626cb495f0f876d2ade0962f38c489967b8e
John <johnoltmans@proton.me>
2025-10-04 15:00:26 +0200

    .

--- bl-themes/bl-github-main/php/page.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-<div class="row">
-<?php Theme::plugins('pageBegin'); ?>
-<?php include(THEME_DIR_PHP.'intro.php'); ?>
-<?php include(THEME_DIR_PHP.'article.php'); ?>
-<?php include(THEME_DIR_PHP.'menu.php'); ?>
-<?php Theme::plugins('pageEnd'); ?>
-</div>
-</div>
