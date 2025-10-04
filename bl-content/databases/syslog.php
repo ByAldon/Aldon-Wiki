ebe84b736e1af7f4db7354c47472708eb795cb2c
John <johnoltmans@proton.me>
2025-10-04 12:54:47 +0200

    .

---
+++ bl-content/databases/syslog.php
@@ -0,0 +1,27 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+[
+    {
+        "date": "2025-10-04 12:52:15",
+        "dictionaryKey": "new-theme-configured",
+        "notes": "bs5docs",
+        "idExecution": "68e0fc5fcbd41",
+        "method": "GET",
+        "username": "admin"
+    },
+    {
+        "date": "2025-10-04 12:52:15",
+        "dictionaryKey": "plugin-deactivated",
+        "notes": "Popeye Thema",
+        "idExecution": "68e0fc5fcbd41",
+        "method": "GET",
+        "username": "admin"
+    },
+    {
+        "date": "2025-10-04 12:52:03",
+        "dictionaryKey": "welcome-to-bludit",
+        "notes": "",
+        "idExecution": "68e0fc53bf919",
+        "method": "POST",
+        "username": "admin"
+    }
+]
---
+++ bl-content/databases/users.php
@@ -0,0 +1,25 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{
+    "admin": {
+        "nickname": "Admin",
+        "firstName": "Beheerder",
+        "lastName": "",
+        "role": "admin",
+        "password": "ed544dada51fddd172775ca6e65153376c72d8a0",
+        "salt": "68e0fc53bf3f2",
+        "email": "",
+        "registered": "2025-10-04 12:52:03",
+        "tokenRemember": "",
+        "tokenAuth": "95afb659afc7b4cf7b03c1beed6c0adc",
+        "tokenAuthTTL": "2009-03-15 14:00",
+        "twitter": "",
+        "facebook": "",
+        "instagram": "",
+        "codepen": "",
+        "linkedin": "",
+        "xing": "",
+        "telegram": "",
+        "github": "",
+        "gitlab": ""
+    }
+}
